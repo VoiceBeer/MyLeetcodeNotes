368f99d5296542e23f31ebd5c7be12c91c1e1b17
VoiceBeer <voicebeer@126.com>
2017-11-09 13:36:26 +0800

    Nov 2nd, #1

--- MyLeetcodeNotes/1TwoSum.swift
+++ MyLeetcodeNotes/1TwoSum.swift
@@ -20,7 +20,7 @@ Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 */
 
-/// 主要是两个 for 循环，第一层选定一个数，第二层来找符合两者相加等于 target 的第二个数，Java 的 Map 能快一些，Swift 的话好像也有，现在还不知道以后说不定会加。
+/// 主要是用两个 for 循环，第一层选定一个数，第二层来找符合两者相加等于 target 的第二个数，Java 的 Map 能快一些，Swift 的话好像也有，现在还不知道以后说不定会加。
 /// Two (for-loop)s. The first loop is to choose a number in the given array, the second loop is to find the number that fits the numToFind which is Target minus first number. In Java, use Map() can improve the efficency, I don't know how to use Map() in Swift, maybe recode this answer in the future.
 
 class Solution_1 {
