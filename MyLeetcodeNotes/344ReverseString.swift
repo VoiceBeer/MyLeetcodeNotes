94cde3aa55e0f65f5ee3d164b1f947402ec4e760
VoiceBeer <370396850@qq.com>
2017-12-05 18:05:40 +0800

    Nov 5th, #344

---
+++ MyLeetcodeNotes/344ReverseString.swift
@@ -0,0 +1,29 @@
+//
+//  344ReverseString.swift
+//  MyLeetcodeNotes
+//
+//  Created by VoiceBeer on 2017/12/5.
+//  Copyright © 2017年 VoiceBeer. All rights reserved.
+//
+
+import Foundation
+
+/*
+ Write a function that takes a string as input and returns the string reversed.
+ 
+ Example:
+ Given s = "hello", return "olleh".
+ */
+
+/// 没啥说的
+/// Nothing to say, fxxking ez
+
+class Solution_344 {
+    func reverseString(_ s: String) -> String {
+        return String(s.reversed())
+    }
+}
+
+//let test = "hello"
+//print(Solution_344().reverseString(test))
+
